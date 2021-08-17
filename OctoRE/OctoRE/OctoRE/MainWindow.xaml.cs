45661bf5876454f031387db9994a1beca6a40f1d
Panagiotis Sakaridis <panosxsakar@gmail.com>
2021-08-17 14:17:13 +0300

    added sidebar

--- OctoRE/OctoRE/OctoRE/MainWindow.xaml.cs
+++ OctoRE/OctoRE/OctoRE/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
@@ -25,12 +24,69 @@ namespace OctoRE
     {
         public MainWindow()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void MyButton_Click(object sender, RoutedEventArgs args)
         {
-            myButton.Content = "Clicked";
+            //myButton.Content = sender.ToString();
+        }
+
+        private void NavigationView_Loaded(object sender, RoutedEventArgs args)
+        {
+            //SetCurrentNavigationViewItem(Views.MainView);
+        }
+
+        public List<NavigationViewItem> GetNavigationViewItems()
+        {
+            var result = new List<NavigationViewItem>();
+            var items = OctoRE.MenuItems.Select(i => (NavigationViewItem)i).ToList();
+            items.AddRange(OctoRE.FooterMenuItems.Select(i => (NavigationViewItem)i));
+            result.AddRange(items);
+
+            foreach (NavigationViewItem mainItem in items)
+            {
+                result.AddRange(mainItem.MenuItems.Select(i => (NavigationViewItem)i));
+            }
+
+            return result;
+        }
+
+        public List<NavigationViewItem> GetNavigationViewItems(
+            Type type)
+        {
+            return GetNavigationViewItems().Where(i => i.Tag.ToString() == type.FullName).ToList();
+        }
+
+        public List<NavigationViewItem> GetNavigationViewItems(
+            Type type,
+            string title)
+        {
+            return GetNavigationViewItems(type).Where(ni => ni.Content.ToString() == title).ToList();
+        }
+
+        public void SetCurrentNavigationViewItem(NavigationViewItem item)
+        {
+            if (item == null)
+            {
+                return;
+            }
+
+            if (item.Tag == null)
+            {
+                return;
+            }
+
+            ContentFrame.Navigate(
+            Type.GetType(item.Tag.ToString()),
+            item.Content);
+            OctoRE.Header = item.Content;
+            OctoRE.SelectedItem = item;
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            SetCurrentNavigationViewItem(args.SelectedItemContainer as NavigationViewItem);
         }
     }
 }
