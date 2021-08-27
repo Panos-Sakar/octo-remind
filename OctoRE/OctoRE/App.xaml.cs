54e501317cfd8595755bc344c65ed6772218985e
Panagiotis Sakaridis <panosxsakar@gmail.com>
2021-08-27 16:48:11 +0300

    Created Title bar

--- OctoRE/OctoRE/App.xaml.cs
+++ OctoRE/OctoRE/App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace OctoRE
 {
@@ -16,6 +10,7 @@ namespace OctoRE
         protected override void OnStartup(StartupEventArgs e)
         {
             Window mainWindow = new MainWindow();
+            Application.Current.MainWindow = mainWindow;
             mainWindow.Show();
 
             base.OnStartup(e);
--- OctoRE/OctoRE/MainWindow.xaml.cs
+++ OctoRE/OctoRE/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OctoRE
 {
@@ -23,6 +12,24 @@ namespace OctoRE
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnBorderMouseDown(Object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void OnMinimizePressed(Object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void OnClosePressed(Object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
