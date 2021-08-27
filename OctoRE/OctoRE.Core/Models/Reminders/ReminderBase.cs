cacb77509be96c0c3f7be06f8f80519858794e4d
Panagiotis Sakaridis <panosxsakar@gmail.com>
2021-08-27 13:32:28 +0300

    Added Reminder class

---
+++ OctoRE/OctoRE.Core/Models/Reminders/ReminderBase.cs
@@ -0,0 +1,27 @@
+﻿namespace OctoRE.Core.Models
+{
+    public class ReminderBase
+    {
+        private readonly int _interval;
+        private int _elapsed;
+
+        public ReminderBase(int interval)
+        {
+            _interval = interval;
+        }
+
+        public void Update()
+        {
+            _elapsed++;
+            if (_elapsed > _interval)
+            {
+                Remind();
+            }
+        }
+
+        public void Remind()
+        {
+            _elapsed = 0;
+        }
+    }
+}
---
+++ OctoRE/OctoRE.Core/State/Dispatch.cs
@@ -0,0 +1,21 @@
+﻿namespace OctoRE.Core.State
+{
+    public class Dispatch
+    {
+        public static Dispatch _dispatch;
+        public Dispatch Start()
+        {
+            if (_dispatch == null)
+            {
+                _dispatch = this;
+            }
+
+            return _dispatch;
+        }
+
+        public static Dispatch GetDispstch()
+        {
+            return _dispatch;
+        }
+    }
+}
